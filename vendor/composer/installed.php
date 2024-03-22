a8701c983603fbe2016d28ad529e63392070c429
Jim Schiel <jim@artisansoftwareconsulting.com>
2024-03-21 18:50:23 -0700

    Add Composer autoloader files
    Newly added autoloader files managed by Composer. These files handle PSR-4 and PSR-0 autoloading for classes in the project. Also included autoloading for specific development requirements. These changes provide better management of project dependencies, and set the path for class loading.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'artisan-software-consulting/auda',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '80620d70ff26e01cb47d5d4e6bcb941f394cb963',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'artisan-software-consulting/auda' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '80620d70ff26e01cb47d5d4e6bcb941f394cb963',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
